6a24c1f18f698e9c61919523cc9d8c29a78a0078
Douglas Duarte <adsdouglasduarte@gmail.com>
2025-11-15 11:59:43 -0300

    git inicial

---
+++ exercicio_es6.js
@@ -0,0 +1,28 @@
+class Pessoa {
+  constructor(nome) {
+    this.nome = nome
+  }
+}
+
+class Aluno extends Pessoa {
+  constructor(nome, nota) {
+    super(nome)
+    this.nota = nota
+  }
+}
+
+const alunos = [
+  new Aluno('Douglas', 9),
+  new Aluno('Ana', 7),
+  new Aluno('Rafael', 5),
+  new Aluno('Marina', 10),
+  new Aluno('Carlos', 3),
+  new Aluno('Fernanda', 8)
+]
+
+const filtrarAprovados = lista => 
+  lista.filter(aluno => aluno.nota >= 6)
+
+const aprovados = filtrarAprovados(alunos)
+
+console.log(aprovados)
